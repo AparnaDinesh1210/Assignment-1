2dad0307a99f1a89811352e0f10fb8a4b78d6e9b
AparnaDinesh1210 <78405144+AparnaDinesh1210@users.noreply.github.com>
2021-02-02 14:10:15 +0530

    Add files via upload

---
+++ SortAscArray.java
@@ -0,0 +1,18 @@
+package Assignment2;
+
+import java.util.*;
+public class SortAscArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] arr = {13, 7, 6, 45, 21, 9, 101, 102, 15, 345};
+		 
+        Arrays.sort(arr);
+ 
+        System.out.printf("Sorted array[] : %s",
+                          Arrays.toString(arr));
+    }
+	}
+
+
