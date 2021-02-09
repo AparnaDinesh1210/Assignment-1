af22b180f0e34801b23018e16c23104f3b887b24
AparnaDinesh1210 <78405144+AparnaDinesh1210@users.noreply.github.com>
2021-02-09 09:25:17 +0530

    Add files via upload

---
+++ TestCustomer.java
@@ -0,0 +1,72 @@
+package assignment3;
+class Address
+{
+	private String addressLine;
+	private String city;
+	public Address(String addressLine, String city) {
+		super();
+		this.addressLine = addressLine;
+		this.city = city;
+	}
+	public String getAddressLine() {
+		return addressLine;
+	}
+	public void setAddressLine(String addressLine) {
+		this.addressLine = addressLine;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	void getAddressDetails()
+	{
+		System.out.println("Address of the employee: " + addressLine + "," +city);
+	}
+}
+class Customer
+{
+	private String customerName;
+	Address residentialAddress;
+	public Customer() {
+		super();
+		customerName="John";
+}
+	public Customer(String customerName, Address residentialAddress) {
+		super();
+		this.customerName = customerName;
+		this.residentialAddress = residentialAddress;
+	}
+        public String getCustomerName() {
+		return customerName;
+	}
+public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+public Address getResidentialAddress() {
+		return residentialAddress;
+	}
+public void setResidentialAddress(Address residentialAddress) {
+		this.residentialAddress = residentialAddress;
+	}
+    void getCustomerDetails()
+	{
+		System.out.println("Customer : " +customerName);
+		System.out.println("Residential Address : "+residentialAddress.getAddressLine() +"," + residentialAddress.getCity());
+	}
+}
+
+public class TestCustomer {
+public static void main(String[] args) {
+		Address add = new Address("1st Main HSR Layout", "Banglore");
+		add.getAddressDetails();
+		
+		Customer ct = new Customer();
+		System.out.println("Customer : " +ct.getCustomerName());
+		System.out.println("Residential Address : "+add.getAddressLine()+","+add.getCity());
+		
+		Customer c = new Customer("John",add);
+		c.getCustomerDetails();
+}
+}
